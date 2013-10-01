cef2c294abe865f11bfca58bae0229b2aa4888fd
petec <peter.claydon@continuumbridge.com>
2013-10-01 16:01:34 +0000

    Pi node socket connects to server

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,26 @@
-var http = require('http');
+// Set up the socket client
+var io = require('socket.io-client'),
+socket = io.connect('54.200.16.244', {
+    port: 4000 
+});
+socket.on('connect', function () { console.log("socket connected"); });
+socket.emit('status', { status: 'ready' });
 
-http.createServer(function(req,resp) {
-    resp.writeHead(200, {"Content-Type": "text/plain"});
-    resp.write("Hello World");
-    resp.end();
-    
-    console.log("sample output to console");
+// Imports for unix socket
+var net = require('net'),
+    fs = require('fs'),
+    sock;
 
-}).listen(8080);
+// Create socket file
+fs.open('/tmp/node.test.sock', 'w+', function(err, fdesc){
+
+    if (err || !fdesc) {
+        throw 'Error: ' + (err || 'No fdesc');
+    }
+
+    // Create socket
+    sock = new net.Socket({ fd : fdesc });
+    console.log(sock);
+});
+
 
