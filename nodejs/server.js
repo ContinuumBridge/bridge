cd11fda09d0b326c2b0a732b2c4bcfb4723e1dc7
petec <peter.claydon@continuumbridge.com>
2013-10-03 17:18:19 +0000

    Sketch of node tcp relay, not tested with bridge yet

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,68 +1,37 @@
 // Set up the socket client
 var io = require('socket.io-client'),
-wSocket = io.connect('54.200.16.244', {
+serverSocket = io.connect('54.200.16.244', {
     port: 4000 
 });
-wSocket.on('connect', function () { console.log("Web socket connected"); });
-wSocket.emit('status', { status: 'ready' });
+serverSocket.on('connect', function () { console.log("Web socket connected"); });
+serverSocket.emit('status', { status: 'ready' });
 
 // Set up the TCP socket for the bridge
 var net = require('net');
-var BRIDGE_HOST = '127.0.0.1';
 var BRIDGE_PORT = 5000;
+//var bridgeSocket;
 
-var bridgeTCPClient = new net.Socket();
-bridgeTCPClient.connect(PORT, HOST, function() {
+net.createServer(function(bridgeSocket) {
 
-    console.log('Local TCP socket connected on: ' + HOST + ':' + PORT);
-    bridgeTCPClient.write('Hi from node!');
-});
+    bridgeSocket.on('connect', function() {
 
-bridgeTCPClient.on('data', function(data) {
+        console.log('Local TCP socket connected on: ' + HOST + ':' + PORT);
+        bridgeSocket.write('Hi from node!');
+    });
+    // Relay data from the TCP socket to the websocket
+    bridgeSocket.on('data', function(data) {
+        
+        console.log('Data from bridge: ' + data);
+        wSocket.emit('data', data);
+    });
     
-    console.log('DATA: ' + data);
-    wSocket.emit('data', data);
+    // Add a 'close' event handler for the bridgeTCPClient socket
+    bridgeSocket.on('close', function() {
+        console.log('Bridge connection closed');
+    });
 });
 
-// Add a 'close' event handler for the bridgeTCPClient socket
-bridgeTCPClient.on('close', function() {
-    console.log('Bridge connection closed');
+serverSocket.on('cmd', function(cmd) {
+    console.log('Command from server: ' + cmd);
+    bridgeSocket.write('cmd', cmd);
 });
-
-/*
-var dgram = require('unix-dgram');
-// Create unix socket
-uSocket = dgram.createSocket('unix_dgram', 'listener');
-//uSocket.bind(fdesc); 
-var message = new Buffer("Test message");
-uSocket.send(message, 0, message.length, '/tmp/testSoc.sock' , function(err, bytes) {
-    console.log('Error connecting:', err, bytes);
-});
-
-/*
-// Imports for unix socket
-var net = require('net'),
-    fs = require('fs'),
-    uSocket;
-
-// Create socket file
-fs.open('/tmp/testSoc.sock', 'w+', function(err, fdesc){
-
-    if (err || !fdesc) {
-        throw 'Error: ' + (err || 'No fdesc');
-    }
-
-    console.log(fdesc);
-
-    // Create socket
-    //uSocket = net.connect('/tmp/testSoc.sock');
-    uSocket = new net.Socket({ fd : fdesc,
-                               type: 'unix', 
-                            });
-
-    uSocket.on('connect', function () { console.log("Unix socket connected"); });
-    console.log(uSocket);
-    
-    uSocket.write("test");
-});
-*/
