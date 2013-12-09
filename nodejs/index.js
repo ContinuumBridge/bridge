40d228a48194700016d03fec6bd7aaa078111d6a
Mark Claydon <mark.claydon@continuumbridge.com>
2013-12-09 18:47:31 +0000

    Added command line arguments and tweaks

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,23 @@
 
+// Get the arguments passed to node
+
+var CONTROLLER_ADDRESS = (process.argv[2]) ? process.argv[2] : 'http://54.200.16.244:8000/api/v1/';
+var BRIDGE_EMAIL = (process.argv[3]) ? process.argv[3] : '3b84fb342d3644b2a6a0f342311fc8e2@continuumbridge.com';
+var BRIDGE_PASSWORD = (process.argv[4]) ? process.argv[4] : 'BEjEu+vAXP4k8Y1qySPdCrN8VrQ5OxewtLfDs58VnIA5VufYahF/QndOSH4cqN7u';
+
+console.log('CONTROLLER_ADDRESS', CONTROLLER_ADDRESS);
+console.log('BRIDGE_EMAIL', BRIDGE_EMAIL);
+console.log('BRIDGE_PASSWORD', BRIDGE_PASSWORD);
+
 var BridgeConcentrator = require('./bridge/bridge_concentrator.js');
 bridgeConcentrator = new BridgeConcentrator(5000);
 
 var controllerAuth = require('./controller/controller_auth.js');
 
-var BRIDGE_EMAIL = '3b84fb342d3644b2a6a0f342311fc8e2@continuumbridge.com';
-var BRIDGE_PASSWORD = 'BEjEu+vAXP4k8Y1qySPdCrN8VrQ5OxewtLfDs58VnIA5VufYahF/QndOSH4cqN7u'
+//var BRIDGE_EMAIL = '3b84fb342d3644b2a6a0f342311fc8e2@continuumbridge.com';
+//var BRIDGE_PASSWORD = 'BEjEu+vAXP4k8Y1qySPdCrN8VrQ5OxewtLfDs58VnIA5VufYahF/QndOSH4cqN7u'
 
-controllerAuth('http://54.200.16.244:8000/api/v1/', BRIDGE_EMAIL, BRIDGE_PASSWORD).then(function(sessionID) {
+controllerAuth(CONTROLLER_ADDRESS, BRIDGE_EMAIL, BRIDGE_PASSWORD).then(function(sessionID) {
 
     console.log('controllerAuth returned', sessionID);
 
