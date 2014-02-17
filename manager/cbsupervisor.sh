2a51ded9558edf07dde14f21f0b6f83451bad0d6
PeterClaydon <peter.claydon@continuumbridge.com>
2014-02-17 19:45:37 +0000

    Removed test code that should not have been there

--- manager/cbsupervisor.sh
+++ manager/cbsupervisor.sh
@@ -5,8 +5,7 @@ if [ -f ../thisbridge/thisbridge.sh ]; then
     # Must source so that exports affect the parent script
     source ../thisbridge/thisbridge.sh
     export PYTHONPATH='/home/bridge/bridge/lib'
-    #./cbsupervisor.py >> ../thisbridge/bridge.log 2>&1 &
-    ./cbsupervisor.py
+    ./cbsupervisor.py >> ../thisbridge/bridge.log 2>&1 &
 else
     echo "thisbridge.sh file does not exist"
     exit
