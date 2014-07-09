1b419692ac78388a403074c05a5f62e8c3f2bf00
PeterClaydon <peter.claydon@continuumbridge.com>
2014-07-09 12:47:58 +0100

    Moved call to removeSecondarySockets from startElements to startAll

--- manager/cbmanager.py
+++ manager/cbmanager.py
@@ -114,7 +114,6 @@ class ManageBridge:
         return mgrSocs
 
     def startElements(self):
-        self.removeSecondarySockets()
         els = [{"id": "conc",
                 "socket": "skt-mgr-conc",
                 "exe": CONCENTRATOR_PATH
@@ -179,6 +178,7 @@ class ManageBridge:
 
     def startAll(self):
         self.states("starting")
+        self.removeSecondarySockets()
         # Manager sockets may already exist. If so, delete them
         mgrSocs = self.listMgrSocs()
         for s in mgrSocs:
